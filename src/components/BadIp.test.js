190229d0034353e8c7757c6bc769aaf0ac7a6266
Christopher Folmar <c.folmar@outlook.com>
2018-06-05 18:34:44 -0400

    Update BadIp Test

--- src/components/BadIp.test.js
+++ src/components/BadIp.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BadIp from './BadIp';
+import { shallow, mount } from 'enzyme';
+import { mountToJson } from 'enzyme-to-json'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<BadIp />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const address = "1.2.3.4";
+
+describe('BadIp', () => {
+
+	it('renders fully without crashing', () => {
+		const component = mount(<BadIp address={address} />);
+		component.unmount();
+	});
+
+	it('full snapshot rendering', () => {
+		const component = mount(
+			<BadIp />
+			);
+		expect(mountToJson(component)).toMatchSnapshot();
+		component.unmount();
+	});
+
+	it('renders populated element', () => {
+		const component =  mount(<BadIp address={address} />);
+		const populatedElement = <h4>{address}</h4>;
+		expect(component.contains(populatedElement)).toEqual(true);
+		component.unmount();
+	});
 });
