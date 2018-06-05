213e776326e540b2295435f0a4afc8fbd51974ba
Christopher Folmar <c.folmar@outlook.com>
2018-06-04 22:06:28 -0400

    Add SearchInput Component & Test
    -add SearchInput component & implementation
    -add SearchInput test

---
+++ src/components/SearchInput.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+
+class SearchInput extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = 
+		{
+			fieldVal: ""
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({fieldVal: event.target.value});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault()
+		this.props.onSubmission(this.state.fieldVal)
+	}
+
+	render(){
+
+		var blacklisted = {
+              color: 'black'
+            };
+
+		return(
+			<header className="masthead text-white text-center">
+				<div className="overlay"></div>
+					<div className="container">
+						<div className="row">
+							<div className="col-xl-9 mx-auto">
+							<h2 className="mb-5">
+								Verify if an IP Address is <span style={blacklisted}>blacklisted</span>
+							</h2>
+						</div>
+						<div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+							<form onSubmit={this.handleSubmit}>
+								<div className="form-row">
+									<div className="col-12 col-md-9 mb-2 mb-md-0">
+											<input 
+												type="text" 
+												className="form-control form-control-lg" 
+												placeholder="Enter IP Address to verify..."
+												onChange={this.handleChange}
+												value={this.state.fieldVal}
+												pattern="\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b"
+												title="1.2.3.4"
+											/>
+									</div>
+									<div className="col-12 col-md-3">
+										<button type="submit" className="btn btn-block btn-lg btn-primary">Search</button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</header>
+			);
+	}
+}
+
+export default SearchInput;
