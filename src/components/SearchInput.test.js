a63be373bfd4ed86c693872d13eba99e5e400685
Christopher Folmar <c.folmar@outlook.com>
2018-06-05 18:36:21 -0400

    Update SearchInput Test

--- src/components/SearchInput.test.js
+++ src/components/SearchInput.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import { mountToJson } from 'enzyme-to-json'
 import SearchInput from './SearchInput';
+import { spy } from 'sinon';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SearchInput />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('SearchInput', () => {
+
+	it('renders fully without crashing', () => {
+		const component = mount(<SearchInput onSubmission={[Function]} />);
+		component.unmount();
+	});
+
+	it('full snapshot rendering', () => {
+		const component = mount(
+			<SearchInput onSubmission={[Function]} />
+			);
+		expect(mountToJson(component)).toMatchSnapshot();
+		component.unmount();
+	});
+
+	it('renders submit button', () => {
+		const onSubmit = spy();
+		const component = mount(<SearchInput onSubmission={onSubmit} />);
+		const submitButton = <div className="col-12 col-md-3">
+								<button type="submit" className="btn btn-block btn-lg btn-primary">Search</button>
+							</div>;
+		expect(component.contains(submitButton)).toEqual(true);
+		component.unmount();
+	});
+
+	it('handles submit with submit event on form', () => {
+		const onSubmit = spy();
+		const component = mount(<SearchInput onSubmission={onSubmit} />);
+		const form = component.find('form');
+		form.simulate('submit');
+		expect(onSubmit.called).toEqual(true);
+		component.unmount();
+	});
+
+	it('handles submit with click event on button', () => {
+		const onSubmit = spy();
+		const component = mount(<SearchInput onSubmission={onSubmit} />);
+		const button = component.find('button');
+		button.simulate('submit');
+		expect(onSubmit.called).toEqual(true);
+		component.unmount();
+	});
+
 });
+
