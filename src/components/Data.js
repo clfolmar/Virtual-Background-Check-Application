0d6cba20cc4b37f09c201e176f22e9bc750b2a30
Christopher Folmar <c.folmar@outlook.com>
2018-06-05 00:48:00 -0400

    Programatically assign each Data Component a relevant icon
    -add icon property to Data Component
    -pass desired icon through icon property from DataWrapper component to Data component

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react'
 class Data extends Component {
 	 render(){
 	 	const header = this.props.header,
+	 			icon = this.props.icon,
         		data = this.props.data;
 
 		return(
 	            <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
 	              <div className="features-icons-icon d-flex">
-	                <i className="icon-list m-auto text-primary"></i>
+	                <i className={icon +" m-auto text-primary"}></i>
 	              </div>
 	              <h5>{header}</h5>
 	              <p className="mb-0">{data}</p>
--- src/components/DataWrapper.js
+++ src/components/DataWrapper.js
@@ -34,60 +34,59 @@ class DataWrapper extends Component {
                       <div className="row">
 
                         <div className="col-lg-3">
-                          <Data header="Hostname" data={hostname} />
+                          <Data header="Hostname" icon="icon-list" data={hostname} />
                         </div>
 
                         <div className="col-lg-3">
-                          <Data header="Continent" data={continent} />
+                          <Data header="Continent" icon="icon-map" data={continent} />
                         </div>
 
                         <div className="col-lg-3">
-                          <Data header="Country" data={country} />
+                          <Data header="Country" icon="icon-home" data={country} />
                         </div>
 
                         <div className="col-lg-3">
-                          <Data header="Region" data={region} />
+                          <Data header="Region" icon="icon-direction" data={region} />
                         </div>
                         
                       </div>
                       <div className="row">
 
                         <div className="col-lg-3">
-                          <Data header="City" data={city} />
+                          <Data header="City" icon="icon-vector" data={city} />
                         </div>
 
                         <div className="col-lg-3">
-                          <Data header="Postal Code" data={postal} />
+                          <Data header="Postal Code" icon="icon-envelope" data={postal} />
                         </div>
 
                         <div className="col-lg-3">
-                          <Data header="Latitude, Longitude" data={latitudeLongitude} />
+                          <Data header="Latitude, Longitude" icon="icon-globe" data={latitudeLongitude} />
                         </div>
 
                         <div className="col-lg-3">
-                          <Data header="Time Zone" data={timeZone} />
+                          <Data header="Time Zone" icon="icon-clock" data={timeZone} />
                         </div>
 
-                      </div>
+                      </div>    
                       <h4>Autonomous System</h4>
                       <div className="row">
 
                         <div className="col-lg-4">
-                          <Data header="Time Zone" data={timeZone} />
+                          <Data header="Number" icon="icon-credit-card" data={asNumber} />
                         </div>
 
                         <div className="col-lg-4">
-                          <Data header="Name" data={asName} />
+                          <Data header="Name" icon="icon-screen-desktop" data={asName} />
                         </div>
 
                         <div className="col-lg-4">
-                          <Data header="Country" data={this.props.asCountry} />
+                          <Data header="Country" icon="icon-globe-alt" data={asCountry} />
                         </div>
-        
-                      </div>
 
                     </div>
-                  </section>
+                  </div>
+                </section>
 
             );
         }
@@ -106,38 +105,38 @@ class DataWrapper extends Component {
                 <div className="row">
 
                   <div className="col-lg-3">
-                    <Data header="Hostname" data={hostname} />
+                    <Data header="Hostname" icon="icon-list" data={hostname} />
                   </div>
 
                   <div className="col-lg-3">
-                    <Data header="Continent" data={continent} />
+                    <Data header="Continent" icon="icon-map" data={continent} />
                   </div>
 
                   <div className="col-lg-3">
-                    <Data header="Country" data={country} />
+                    <Data header="Country" icon="icon-home" data={country} />
                   </div>
 
                   <div className="col-lg-3">
-                    <Data header="Region" data={region} />
+                    <Data header="Region" icon="icon-direction" data={region} />
                   </div>
                   
                 </div>
                 <div className="row">
 
                   <div className="col-lg-3">
-                    <Data header="City" data={city} />
+                    <Data header="City" icon="icon-vector" data={city} />
                   </div>
 
                   <div className="col-lg-3">
-                    <Data header="Postal Code" data={postal} />
+                    <Data header="Postal Code" icon="icon-envelope" data={postal} />
                   </div>
 
                   <div className="col-lg-3">
-                    <Data header="Latitude, Longitude" data={latitudeLongitude} />
+                    <Data header="Latitude, Longitude" icon="icon-globe" data={latitudeLongitude} />
                   </div>
 
                   <div className="col-lg-3">
-                    <Data header="Time Zone" data={timeZone} />
+                    <Data header="Time Zone" icon="icon-clock" data={timeZone} />
                   </div>
 
                 </div>    
@@ -145,15 +144,15 @@ class DataWrapper extends Component {
                 <div className="row">
 
                   <div className="col-lg-4">
-                    <Data header="Number" data={asNumber} />
+                    <Data header="Number" icon="icon-credit-card" data={asNumber} />
                   </div>
 
                   <div className="col-lg-4">
-                    <Data header="Name" data={asName} />
+                    <Data header="Name" icon="icon-screen-desktop" data={asName} />
                   </div>
 
                   <div className="col-lg-4">
-                    <Data header="Country" data={asCountry} />
+                    <Data header="Country" icon="icon-globe-alt" data={asCountry} />
                   </div>
   
                 </div>
