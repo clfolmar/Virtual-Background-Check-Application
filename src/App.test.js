b09f57999f42e340f30ec09b385c8ba9844a9652
Christopher Folmar <c.folmar@outlook.com>
2018-06-05 18:34:11 -0400

    Update App.Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme'
+import { mountToJson } from 'enzyme-to-json'
 import App from './App';
+import Container from './components/Container';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+	it('renders without crashing', () => {
+		shallow(<App />);
+	});
+
+	it('renders Container component', () => {
+		const component = shallow(<App />);
+		const container = <Container />;
+		// expect (wrapper.contains(container)).to.equal(true);
+		expect(component.contains(container)).toEqual(true);
+	});
+
+	it('full snapshot rendering', () => {
+		const component = mount(
+			<App />
+			);
+		expect(mountToJson(component)).toMatchSnapshot();
+		component.unmount();
+	});
 });
