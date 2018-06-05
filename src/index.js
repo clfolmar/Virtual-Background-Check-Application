4cadf8e377bd841e494364309f3d7cc21855344b
Christopher Folmar <c.folmar@outlook.com>
2018-06-05 00:45:51 -0400

    Update Bootstrap Theme Implementation
    -moved vendor folder of included bootstrap theme into the src folder so that the relevent css files could properly be imported within the application's entry point; index.js
    
    -added custom alterations to the theme to shrink icon sizes and space between elements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// Bootstrap core CSS -->
+import './vendor/bootstrap/css/bootstrap.min.css'
+// Custom fonts for this template
+import './vendor/font-awesome/css/font-awesome.min.css'
+import './vendor/simple-line-icons/css/simple-line-icons.css'
+// Custom styles for this template
+import './css/landing-page.min.css'
+
 require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
